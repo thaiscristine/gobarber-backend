29808fb192f443c1a88b07ab02709659415447bc
Thais Cristine <this.cristine06@gmail.com>
2020-11-19 16:27:59 +0200

    add: cryptography of password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,7 +11,19 @@ usersRouter.post('/', async (request, response) => {
     const createUser = new CreateUserService();
 
     const user = await createUser.execute({ name, email, password });
-    return response.json(user);
+
+    // last update of typescript doesn't allow us make it if it's not a any value
+    // delete user.password;
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
