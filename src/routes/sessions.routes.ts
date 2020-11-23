c72ce47913817c0a61b9d17215a0a34a425ac9a2
Thais Cristine <this.cristine06@gmail.com>
2020-11-23 23:26:15 +0200

    add: handling known and unknown errors

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -4,25 +4,21 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({ email, password });
+  const { user, token } = await authenticateUser.execute({ email, password });
 
-    const userWithoutPassword = {
-      name: user.name,
-      id: user.id,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+  const userWithoutPassword = {
+    name: user.name,
+    id: user.id,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-    return response.json({ userWithoutPassword, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json({ userWithoutPassword, token });
 });
 
 export default sessionsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
-// src/server.ts
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
 import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -15,6 +17,22 @@ app.use('/files', express.static(uploadConfig.directory));
 
 // passando todas as rotas para serem escutadas
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Mãe ta on 🤸🏽‍♀️ on port 3333!');
